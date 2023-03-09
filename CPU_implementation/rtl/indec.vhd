2a7607157ab4fd4423d35dca01f0d7c104c0a70e
David Mihola <david.mihola@sperling.cz>
2023-03-09 23:30:37 +0100

    main-WIP on instruction decoder

--- CPU_implementation/rtl/indec.vhd
+++ CPU_implementation/rtl/indec.vhd
@@ -42,4 +42,64 @@ end indec;
 
 architecture rtl of indec is
 begin
+	process(iword) is
+	begin
+		opc <= iword(27 downto 23);
+		pccontr <= (others => '0');
+		inop <= '0';
+		outop <= '0';
+		loadop <= '0';
+		storeop <= '0';
+		dmemop <= '0';
+		selxres <= '0';
+		dpma <= '0';
+		epma <= '0';
+
+		case iword(31 downto 28) is
+			when "0000" => -- SWI
+				if iword(27 downto 23) = "00010" then
+				   pccontr <= "01" & "000000000";
+				end if;
+			when "1010" => -- JAL
+				pccontr <= "101" & "00000000";
+			when "1011" => -- JREG
+				pccontr <= "0001" & "0000000";
+			when "1100" => -- RETI
+				pccontr <= "00001" & "000000";
+			when "0001" => -- ENI
+				pccontr <= "000001" & "00000";
+			when "0100" => -- DEI
+				pccontr <= "0000001" & "0000";
+			when "1101" => -- SISA
+				pccontr <= "10000001" & "000";
+			when "0111" => -- BOV
+				pccontr <= "100000001" & "00";
+			when "0110" => -- BEQZ
+				pccontr <= "1000000001" & "0";
+			when "0101" => -- BNEZ
+				pccontr <= "10000000001";
+			
+			when "0010" => -- IN
+				inop <= '1';
+				selxres <= '1';
+			when "0011" => -- OUT
+				outop <= '1';
+				selxres <= '1';
+			when "1000" => -- LOAD
+				loadop <= '1';
+				dmemop <= '1';
+				selxres <= '1';
+			when "1001" => -- STORE
+				storeop <= '1';
+				dmemop <= '1';
+				selxres <= '1';
+			when "1110" => -- DPMA
+				dpma <= '1';
+			when "1111" => -- EPMA
+				epma <= '1';
+
+			when others =>
+				pccontr <= (others => '0');
+		end case;
+	end process;
 end rtl;
