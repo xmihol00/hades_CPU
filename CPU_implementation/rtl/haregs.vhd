3f459c971d9af2c949e9022d4271e2fa6f231b1b
David Mihola <david.mihola@sperling.cz>
2023-03-11 23:35:34 +0100

    main-haregs implemented using compoments

--- CPU_implementation/rtl/haregs.vhd
+++ CPU_implementation/rtl/haregs.vhd
@@ -11,6 +11,104 @@ library ieee;
 	use ieee.numeric_std.all;
 library work;
 	use work.hadescomponents.all;
+
+entity multiplexer_8to1 is 
+	port(	
+		-- input signals
+		sig1    : in std_logic_vector(31 downto 0);
+		sig2    : in std_logic_vector(31 downto 0);
+		sig3    : in std_logic_vector(31 downto 0);
+		sig4    : in std_logic_vector(31 downto 0);
+		sig5    : in std_logic_vector(31 downto 0);
+		sig6    : in std_logic_vector(31 downto 0);
+		sig7    : in std_logic_vector(31 downto 0);
+		
+		-- selector
+		sel		: in std_logic_vector(2 downto 0);
+
+		-- output signal
+		wout	: out std_logic_vector(31 downto 0)
+	);
+end multiplexer_8to1;
+
+architecture rtl of multiplexer_8to1 is
+begin
+	process(sig1, sig2, sig3, sig4, sig5, sig6, sig7, sel) is
+	begin
+		case sel is
+			when "001" => wout <= sig1;
+			when "010" => wout <= sig2;
+			when "011" => wout <= sig3;
+			when "100" => wout <= sig4;
+			when "101" => wout <= sig5;
+			when "110" => wout <= sig6;
+			when "111" => wout <= sig7;
+			when others => wout <= (others => '0');
+		end case;
+	end process;
+end rtl;
+
+library ieee;
+	use ieee.std_logic_1164.all;
+	use ieee.numeric_std.all;
+library work;
+	use work.hadescomponents.all;
+
+entity demultiplexer_1to8 is 
+	port(
+		-- enables output signal to be written
+		enable	: in std_logic;
+		reset	: in std_logic;
+
+		-- input signal
+		rin		: in std_logic_vector(31 downto 0);
+		
+		-- selector
+		sel		: in std_logic_vector(2 downto 0);
+
+		-- output signals
+		sig1    : out std_logic_vector(31 downto 0);
+		sig2    : out std_logic_vector(31 downto 0);
+		sig3    : out std_logic_vector(31 downto 0);
+		sig4    : out std_logic_vector(31 downto 0);
+		sig5    : out std_logic_vector(31 downto 0);
+		sig6    : out std_logic_vector(31 downto 0);
+		sig7    : out std_logic_vector(31 downto 0)
+	);
+end demultiplexer_1to8;
+
+architecture rtl of demultiplexer_1to8 is
+begin
+	process(reset, rin, sel, enable) is
+	begin
+		if reset = '1' then
+			sig1 <= (others => '0');
+			sig2 <= (others => '0');
+			sig3 <= (others => '0');
+			sig4 <= (others => '0');
+			sig5 <= (others => '0');
+			sig6 <= (others => '0');
+			sig7 <= (others => '0');
+		elsif enable = '1' then
+			case sel is
+				when "001" => sig1 <= rin;
+				when "010" => sig2 <= rin;
+				when "011" => sig3 <= rin;
+				when "100" => sig4 <= rin;
+				when "101" => sig5 <= rin;
+				when "110" => sig6 <= rin;
+				when "111" => sig7 <= rin;
+				when others => null;
+			end case;
+		end if;
+	end process;
+end rtl;
+
+library ieee;
+	use ieee.std_logic_1164.all;
+	use ieee.numeric_std.all;
+library work;
+	use work.hadescomponents.all;
 	
 entity haregs is
 	port (
@@ -33,36 +131,107 @@ entity haregs is
 	);
 end haregs;
 
-architecture rtl of haregs is -- currently the same code as in 'haregs_8regs.vhd' file
-	type RegisterFile_t is array(0 to 7) of std_logic_vector(31 downto 0); -- register file of 8 32-bit registers
-	signal registers : RegisterFile_t;
-	signal written : std_logic := '0'; -- Ensure processes that read reagisters are notified when any register changes.
-	constant r0_idx : integer := 0;    -- Index of the fisrt register that cannot be written and always returns 0.
+architecture rtl of haregs is 
+	component multiplexer_8to1 is
+		port(
+			-- input signals
+			sig1    : in std_logic_vector(31 downto 0);
+			sig2    : in std_logic_vector(31 downto 0);
+			sig3    : in std_logic_vector(31 downto 0);
+			sig4    : in std_logic_vector(31 downto 0);
+			sig5    : in std_logic_vector(31 downto 0);
+			sig6    : in std_logic_vector(31 downto 0);
+			sig7    : in std_logic_vector(31 downto 0);
+
+			-- selector
+			sel		: in std_logic_vector(2 downto 0);
+
+			-- output signal
+			wout	: out std_logic_vector(31 downto 0)
+		);
+	end component;
+
+	component demultiplexer_1to8 is 
+		port(
+			-- enables output signal to be written
+			enable	: in std_logic;
+			reset	: in std_logic;
+
+			-- input signal
+			rin		: in std_logic_vector(31 downto 0);
+
+			-- selector
+			sel		: in std_logic_vector(2 downto 0);
+
+			-- output signals
+			sig1    : out std_logic_vector(31 downto 0);
+			sig2    : out std_logic_vector(31 downto 0);
+			sig3    : out std_logic_vector(31 downto 0);
+			sig4    : out std_logic_vector(31 downto 0);
+			sig5    : out std_logic_vector(31 downto 0);
+			sig6    : out std_logic_vector(31 downto 0);
+			sig7    : out std_logic_vector(31 downto 0)
+		);
+	end component;
+
+	signal reg1 : std_logic_vector(31 downto 0);
+	signal reg2 : std_logic_vector(31 downto 0);
+	signal reg3 : std_logic_vector(31 downto 0);
+	signal reg4 : std_logic_vector(31 downto 0);
+	signal reg5 : std_logic_vector(31 downto 0);
+	signal reg6 : std_logic_vector(31 downto 0);
+	signal reg7 : std_logic_vector(31 downto 0);
+
+	signal enable : std_logic := '0';
 begin
 	-- All writes to registers must be in a single process, otherwise the value inside registers will be undefined.
-	process(clk, reset, regwrite, wopadr) is
+	process(clk, regwrite) is
 	begin
-		if reset = '1' then -- reset values of registers to 0
-			for i in registers'range loop
-				registers(i) <= (others => '0');
-			end loop;
-			written <= not written; -- notify processes about a change in the register file
-		elsif rising_edge(clk) and regwrite = '1' then -- synchronously write a supplied value to a given valid register
-			registers(to_integer(unsigned(wopadr))) <= wop;
-			registers(r0_idx) <= (others => '0'); -- make sure r0 is always 0
-			written <= not written; -- notify processes about a change in the register file
+		if rising_edge(clk) and regwrite = '1' then -- synchronously write a supplied value to a given valid register
+			enable <= '1';
+		else
+			enable <= '0';
 		end if;
 	end process;
+	
+	-- input
+	WRITE: demultiplexer_1to8 port map(
+		sig1 => reg1,
+		sig2 => reg2,
+		sig3 => reg3,
+		sig4 => reg4,
+		sig5 => reg5,
+		sig6 => reg6,
+		sig7 => reg7,
+		sel  => wopadr,
+		rin  => wop,
+		enable => enable,
+		reset => reset
+	);
 
 	-- output A
-	process(written, aopadr) is
-	begin
-		aop <= registers(to_integer(unsigned(aopadr)));
-	end process;
+	outA: multiplexer_8to1 port map(
+		sig1 => reg1,
+		sig2 => reg2,
+		sig3 => reg3,
+		sig4 => reg4,
+		sig5 => reg5,
+		sig6 => reg6,
+		sig7 => reg7,
+		sel  => aopadr,
+		wout => aop
+	);
 	
 	-- output B
-	process(written, bopadr) is
-	begin
-		bop <= registers(to_integer(unsigned(bopadr)));
-	end process;
+	outB: multiplexer_8to1 port map(
+		sig1 => reg1,
+		sig2 => reg2,
+		sig3 => reg3,
+		sig4 => reg4,
+		sig5 => reg5,
+		sig6 => reg6,
+		sig7 => reg7,
+		sel  => bopadr,
+		wout => bop
+	);
 end rtl;
